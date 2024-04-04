4ce8b04ebc72121200e9cb6ba12640055369b8d7
박혜림 <parkhyerim10@gmail.com>
2024-04-04 23:27:02 +0900

    [test] 로그인 회원가입 fe 파일

---
+++ app/src/main/java/com/example/supporty/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.supporty;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+
+}
+}
